24cf6641e337ddc2950eee57869f2c882fa26023
tUNGlEon00 <tungleon24@gmail.com>
2024-01-27 14:12:44 +0700

    lethanhtung

---
+++ Baithuchanh/src/slide47.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class slide47 {
+    public static void main(String[] args) throws Exception {
+        Scanner 1 = new Scanner(System.in);
+        int so, Tong=0;
+        while (Tong<100)
+
+
+    }
+    
+}
